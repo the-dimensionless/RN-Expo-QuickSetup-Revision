f441fde82317b10d1f4717dfb76f60b65de45b50
Sumit Singh <sumitsingh@Nagarros-MacBook-Pro.local>
2021-07-11 02:20:58 +0530

    basic navigation setup & screens added

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import ImageScreen from './src/screens/ImageScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi There!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- readMe.md
+++ readMe.md
@@ -18,3 +18,12 @@
 1. expo init <project_name>
 2. expo start
 3. Through metro server, click on Run on Android emulato, IOS emulator or real device through tunneling and QR code Scan
+
+### Adding navigation packages using expo itself
+1. expo install @react-native-community/masked-view react-native-gesture-handler react-native-screens 
+2. expo install @react-navigation/native @react-navigation/stack
+
+### Begin with navigation
+1. import { NavigationContainer } from '@react-navigation/native';
+2. Two ways to build navigation -> programmatically (react-native apis) vs declaratively (JSX)
+3. import { createStackNavigator } from '@react-navigation/stack';
